1f79d5c0cf6f6690239dd5e9c2b96b3cffa32902
Tfauves <tfauvelle@gmail.com>
2021-12-17 20:52:35 -0500

    student put mapping for testing.

--- src/main/java/com/careerdevs/openshit/controllers/StudentController.java
+++ src/main/java/com/careerdevs/openshit/controllers/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,6 +26,18 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<Student> createStudent(@RequestBody Student newStudent) {
         return new ResponseEntity<>(repository.save(newStudent), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody Student updateStudent(@PathVariable Long id, @RequestBody Student updateData) {
+        Student student = repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (updateData.getFirstName() != null) student.setFirstName(updateData.getFirstName());
+        if (updateData.getLastName() != null) student.setLastName(updateData.getLastName());
+        if (updateData.getCohort() != null) student.setCohort(updateData.getCohort());
+
+        return repository.save(student);
+
     }
 
 
